2f55a4a8134083e03f4a538ee3d05d31532ee18b
vidhitkathuria <72202024+vidhitkathuria@users.noreply.github.com>
2023-03-05 03:09:35 +0530

    updated code

---
+++ src/pages/Create.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+const Create = () => {
+  const [error, setError] = useState(null);
+  const [contact, setContact] = useState({
+    name: "",
+    number: "",
+  });
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+    try {
+      const { data } = await axios.post(
+        "http://localhost:5000/contact/create",
+        contact, {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(localStorage.getItem("user")).token}`
+            }
+        }
+      );
+      console.log(data);
+      navigate("/");
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
+  return (
+    <div className="text-center my-4">
+      <h1>Create new Contact</h1>
+      <form
+        action=""
+        className="form-control d-flex flex-column w-25 p-2 mx-auto"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          placeholder="Enter name"
+          className="form-control my-3"
+          onChange={(e) => setContact({ ...contact, name: e.target.value })}
+        />
+        <input
+          type="text"
+          className="form-control mb-3"
+          placeholder="enter contact number"
+          onChange={(e) => setContact({ ...contact, number: e.target.value })}
+        />
+        <button
+          className="form-control btn btn-primary w-25 mx-auto"
+          type="submit"
+        >
+          Create
+        </button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default Create;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,179 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+const Home = () => {
+  const [contacts, setContacts] = useState([]);
+  const [editIdx, setEditIdx] = useState(null);
+  const [editedContact, setEditedcontact] = useState(null);
+  
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(
+        "http://localhost:5000/contact/contacts",
+        {
+          headers: {
+            Authorization: `Bearer ${
+              JSON.parse(localStorage.getItem("user")).token
+            }`,
+          },
+        }
+      );
+      setContacts(data);
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/contact/delete?id=${id}`, {
+        headers: {
+          Authorization: `Bearer ${
+            JSON.parse(localStorage.getItem("user")).token
+          }`,
+        },
+      });
+
+      // deletion done
+      // contact can be removed from ui
+      setContacts(contacts.filter((contact) => contact._id !== id));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleUpdate = async (id) => {
+    try {
+      // api call
+      await axios.patch(
+        `http://localhost:5000/contact/update?id=${id}`,
+        {
+          name: editedContact.name,
+          number: Number(editedContact.number),
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${
+              JSON.parse(localStorage.getItem("user")).token
+            }`,
+          },
+        }
+      );
+
+      // ui update
+      const updatedContactList = [...contacts];
+      updatedContactList.splice(editIdx, 1, editedContact);
+
+      setContacts(updatedContactList);
+
+      setEditIdx(null);
+      setEditedcontact(null);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSubmit = () => {
+    navigate("/create");
+  };
+  return (
+    <div className="mx-4">
+      <div className="d-flex justify-content-between align-items-center w-75 mx-auto my-4">
+        <h1>Contact Book</h1>
+
+        <button
+          onClick={handleSubmit}
+          type="submit"
+          className="btn btn-primary"
+        >
+          Create new Contact
+        </button>
+      </div>
+
+      <table className="table table-bordered w-75 mx-auto">
+        <thead>
+          <tr className="text-center">
+            <th>Contact Name</th>
+            <th>Contact Number</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody className="text-center">
+          {contacts.map((contact, idx) => (
+            <tr key={contact._id}>
+              {editIdx !== idx && <td>{contact.name}</td>}
+              {editIdx !== idx && <td>{contact.number}</td>}
+
+              {editIdx === idx && (
+                <td>
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={editedContact.name}
+                    onChange={(e) =>
+                      setEditedcontact({
+                        ...editedContact,
+                        ["name"]: e.target.value,
+                      })
+                    }
+                  />
+                </td>
+              )}
+              {editIdx === idx && (
+                <td>
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={editedContact.number}
+                    onChange={(e) =>
+                      setEditedcontact({
+                        ...editedContact,
+                        ["number"]: e.target.value,
+                      })
+                    }
+                  />
+                </td>
+              )}
+
+              <td>
+                <div>
+                  {!editedContact && (
+                    <div className="d-flex gap-2 justify-content-center">
+                      <button
+                        onClick={() => {
+                          setEditIdx(idx);
+                          setEditedcontact(contact);
+                        }}
+                        className="btn border"
+                      >
+                        🩹
+                      </button>
+                      <button
+                        onClick={() => handleDelete(contact._id)}
+                        className="btn border"
+                      >
+                        🗑️
+                      </button>
+                    </div>
+                  )}
+
+                  {editedContact && (
+                    <button
+                      onClick={() => handleUpdate(contact._id)}
+                      className="btn border"
+                    >
+                      ✅
+                    </button>
+                  )}
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Home;
